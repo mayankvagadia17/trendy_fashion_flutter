382f3fbd011daa06e54f11262c3943d8b560bce1
mayankvagadiya2023 <mayankvagadia17@gmail.com>
2025-01-07 14:19:52 +0530

    Bottom navigation Color

--- lib/helper/Color.dart
+++ lib/helper/Color.dart
@@ -5,3 +5,4 @@ const Color white = Color(0xffffffff);
 const Color black = Color(0xff000000);
 const Color fontColor = Color(0xff52575C);
 const Color lightWhite = Color(0xffEDEDED);
+const Color bottomNavigationColor = Color(0xff1f2029);
